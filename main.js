152e6eb19e141464e82bd4b7e760f9a91b7edc2f
bogeta11040 <73924098+bogeta11040@users.noreply.github.com>
2021-01-07 06:46:49 +0100

    yes

---
+++ main.js
@@ -0,0 +1,30 @@
+let sentence = document.querySelector('#sentence');
+let res = document.querySelector('#result');
+let submitBtn = document.querySelector('#submitbtn');
+let resetBtn = document.querySelector('#resetbtn');
+
+submitBtn.addEventListener('click', makeResult);
+resetBtn.addEventListener('click', resetText);
+
+function titleCase(str) {
+let regex = /(\w*)'?(\w)/gi;
+let o = [];
+let p = str.match(regex);
+
+
+for (let i = 0; i < p.length; i++) {
+  o.push(p[i][0].toUpperCase() + p[i].slice(1).toLowerCase());
+}
+let result = o.join(" ");
+ return result;
+}
+
+function makeResult() {
+  let str = sentence.value;
+  res.value = titleCase(str);
+}
+
+function resetText() {
+  sentence.value = "";
+  res.value = "";
+}
